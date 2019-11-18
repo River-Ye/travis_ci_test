4d4e0926f62785e4870f09e2c1ac17b23e800a42
River <river800111@gmail.com>
2019-11-18 11:23:45 +0800

    測試 Travis CI 能執行

---
+++ spec/views/tasks/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/new", type: :view do
+  before(:each) do
+    assign(:task, Task.new(
+      :title => "MyString",
+      :content => "MyText"
+    ))
+  end
+
+  it "renders new task form" do
+    render
+
+    assert_select "form[action=?][method=?]", tasks_path, "post" do
+
+      assert_select "input[name=?]", "task[title]"
+
+      assert_select "textarea[name=?]", "task[content]"
+    end
+  end
+end
